d1fbe766b036c7137d39c6ca442d7640d9015ebd
Nikhith Sannidhi <niksami10@gmail.com>
2021-11-28 20:38:13 -0700

    Added code comments for easy understanding

--- test_ticket_viewer.py
+++ test_ticket_viewer.py
@@ -1,42 +1,57 @@
 import unittest
-from unittest import mock
-import ticket_viewer
 import json
-import requests_mock
-from mock import patch
+import ticket_viewer
 import io
-
+import requests
 
 class TestTicketViewer(unittest.TestCase):
 
+    #Test get_creds()
     def test_get_creds_json(self):
         result = ticket_viewer.get_creds()
         self.assertIsNotNone(result)
 
+    #Test if valid details are configured in credentials.json
     def test_get_creds_inner_details(self):
         result = ticket_viewer.get_creds()
         self.assertIn("zendesk.com", result['url'])
         self.assertIn("@", result['user'])
         self.assertIsNotNone(result['auth_token'])
 
+    #Test print menu is not returning anything
     def test_print_menu(self):
         result = ticket_viewer.print_menu()
         self.assertIsNone(result)   
         
+    #Test if Zendesk APIs are working
+    def test_api_working(self):
+        creds = ticket_viewer.get_creds()
 
+        response = requests.get(creds['url']+ "/api/v2/tickets", auth=(creds['user'], creds['auth_token']))
+        self.assertEqual(response.status_code,200)
+
+        response = requests.get(creds['url']+ "/api/v2/tickets/5", auth=(creds['user'], creds['auth_token']))
+        self.assertEqual(response.status_code,200)
+        
+        response = requests.get(creds['url']+ "/api/v2/tickets?page[size]=25", auth=(creds['user'], creds['auth_token']))
+        self.assertEqual(response.status_code,200)
+
+
+    #Test if the returned data is correct.
     def test_get_requested_data(self):
         creds = ticket_viewer.get_creds()
+
+        #Commenting the below as the reviewer's data will differ
+        #self.assertEqual(ticket_viewer.get_requested_data(creds, "/api/v2/tickets/8")['ticket']['requester_id'], 421868146372)    
+        #self.assertEqual(ticket_viewer.get_requested_data(creds, "/api/v2/tickets/11")['ticket']['subject'], "magna reprehenderit nisi est cillum")    
+        #If you want to test the above at your end, please uncomment the above two statements, update the requester ID and subject from your Zendesk data. The given values are from the data at end
         
-        data = ticket_viewer.get_requested_data(creds, "/api/v2/tickets/11")
-        
-        self.assertEqual(ticket_viewer.get_requested_data(creds, "/api/v2/tickets/8")['ticket']['requester_id'], 421868146372)    
-        self.assertEqual(ticket_viewer.get_requested_data(creds, "/api/v2/tickets/11")['ticket']['subject'], "magna reprehenderit nisi est cillum")    
-        self.assertEqual(ticket_viewer.get_requested_data(creds, "/api/v2/tickets/10000")['error'], "RecordNotFound") 
+        self.assertEqual(ticket_viewer.get_requested_data(creds, "/api/v2/tickets/100000000000000")['error'], "RecordNotFound") 
         self.assertEqual(ticket_viewer.get_requested_data(creds, "/api/v2/tickets/0")['error'], "RecordNotFound")    
-        self.assertEqual(ticket_viewer.get_requested_data(creds, "/api/v2/tickets/12345")['description'], "Not found")
+        self.assertEqual(ticket_viewer.get_requested_data(creds, "/api/v2/tickets/1234567890123456789")['description'], "Not found")
         self.assertEqual(ticket_viewer.get_requested_data(creds, "/api/v2/dummy/api")['error'], "InvalidEndpoint")      
 
-
+    #Test if paging API is return less than or equal to 25 tickets
     def test_25_tickets(self):
         creds = ticket_viewer.get_creds()
         data = ticket_viewer.get_requested_data(creds, "/api/v2/tickets?page[size]=25")
@@ -47,12 +62,11 @@ class TestTicketViewer(unittest.TestCase):
             data = ticket_viewer.get_requested_data(creds, "/api/v2/tickets?page[size]=25&page[after]="+data['meta']['after_cursor'])
             self.assertLessEqual(len(data['tickets']), 25)
 
+    #Test if GET all tickets API is return less than or equal to 100 tickets
     def test_100_tickets(self):
         creds = ticket_viewer.get_creds()
         data = ticket_viewer.get_requested_data(creds, "/api/v2/tickets")
         self.assertLessEqual(len(data['tickets']), 100)
-
-
 
 if __name__ == '__main__':
     unittest.main()
--- ticket_viewer.py
+++ ticket_viewer.py
@@ -2,17 +2,23 @@ import requests
 import json
 from tabulate import tabulate
 
+#Load Zendesk credentials from credentials.json
 def get_creds():
     try:
         creds=None
         with open('credentials.json') as f:
             creds = json.load(f)
-        if creds['url']==None or creds['user']==None or creds['auth_token']==None:
-            print("One or more values in the credentials.json is missing")    
-    except:
+        
+        #Error message if any of the required secrets are missing   
+        if creds['url']=='' or creds['user']=='' or creds['auth_token']=='':
+            raise Exception("One or more values in the credentials.json is missing. Please enter all the required secrets.")
+    except Exception as e:
         print("Error while reading credentials.json")
+        print(e)
+        exit()
     return creds
 
+#Display menu with available options
 def print_menu():
     print("\nSelect view options:")
     print("* Press 1  to view all tickets")
@@ -20,27 +26,28 @@ def print_menu():
     print("* Press 3  to list 25 tickets per page")
     print("Type quit to exit")
 
-
+#Make a HTTP API request call and return the response in json 
 def get_requested_data(creds, api_url):
 
     url = creds['url'] + api_url
     user = creds['user']
     pwd = creds['auth_token']
 
+    print(api_url)
     response = requests.get(url, auth=(user, pwd))
-    
+    print(response)
     data = response.json()
 
+    #Handle error responses
     if response.status_code != 200:
         if data['error']=='RecordNotFound':
             print("Given ticket ID is not found.")
         else:    
             print('Error while making GET request: ', data['error'])
     
-    #print(data)
     return data
 
-
+#Prompt user for a ticket ID and display its ticket details
 def display_a_ticket(creds):
     ticket_id = input("Enter ticket number:")
     try:
@@ -49,13 +56,17 @@ def display_a_ticket(creds):
     except:
         return
 
+#Display all ticket details in a list 
 def display_all_tickets(creds):
     try:
+        print(creds)
         data = get_requested_data(creds, "/api/v2/tickets")
+        print(data)
         display_tabulated_format(data['tickets'])
     except:
         return    
-    
+
+#Display ticket details in a tabulated form
 def display_tabulated_format(tickets):
     table =[]
     for ticket in tickets:
@@ -63,50 +74,48 @@ def display_tabulated_format(tickets):
         table.append(row)
     print(tabulate(table, headers=["Ticket ID", "Requester ID", "Assignee ID", "Status", "Subject"]))
 
+#Display 25 ticket details with a paging option
 def display_25_tickets(creds):
-    data = get_requested_data(creds, "/api/v2/tickets?page[size]=25")
-    display_tabulated_format(data['tickets'])
-
-    #print(len(data))
-    #print(len(data['tickets']))
-
-    start_counter=0
-    
-    
-    while True:
-        nav_string = ""
-
-        #Showing the 'previous' option only if user is not on the first page 
-        if start_counter != 0:
-            nav_string = "previous page (p)"
-
-        #Showing the 'next' option only if there are more pages available. 
-        if data['meta']['has_more']:
-            if nav_string !="":
-                nav_string = nav_string + " or next page (n)"
-            else:
-                nav_string = "next page (n)"                
-
-        #Always show return to main menu option
-        nav_string = nav_string + " or return to main menu (r)?"
-    
-        navigate = input("\nDo you want to navigate to " + nav_string + " Enter the letter given in the parenthesis: ")
-
-        if navigate == 'p' and start_counter != 0:
-            data = get_requested_data(creds, "/api/v2/tickets.json?page[size]=25&page[before]="+data['meta']['before_cursor'])
-            display_tabulated_format(data['tickets']) 
-            start_counter = 0 if start_counter<25 else start_counter - 25
-
-        elif navigate == 'n' and data['meta']['has_more']:
-            data = get_requested_data(creds, "/api/v2/tickets.json?page[size]=25&page[after]="+data['meta']['after_cursor'])
-            display_tabulated_format(data['tickets'])
-            start_counter = start_counter + 25
+    try:
+        data = get_requested_data(creds, "/api/v2/tickets?page[size]=25")
+        display_tabulated_format(data['tickets'])
+        start_counter=0
         
-        elif navigate == 'r':
-            return    
-        
-        else:    
-            print("Invalid input.")
+        while True:
+            nav_string = ""
+
+            #Showing the 'previous' option only if user is not on the first page 
+            if start_counter != 0:
+                nav_string = "previous page (p)"
+
+            #Showing the 'next' option only if there are more pages available. 
+            if data['meta']['has_more']:
+                if nav_string !="":
+                    nav_string = nav_string + " or next page (n)"
+                else:
+                    nav_string = "next page (n)"                
+
+            #Always show return to main menu option
+            nav_string = nav_string + " or return to main menu (r)?"
+            navigate = input("\nDo you want to navigate to " + nav_string + " Enter the letter given in the parenthesis: ")
+
+            if navigate == 'p' and start_counter != 0:
+                data = get_requested_data(creds, "/api/v2/tickets.json?page[size]=25&page[before]="+data['meta']['before_cursor'])
+                display_tabulated_format(data['tickets']) 
+                start_counter = 0 if start_counter<25 else start_counter - 25
+
+            elif navigate == 'n' and data['meta']['has_more']:
+                data = get_requested_data(creds, "/api/v2/tickets.json?page[size]=25&page[after]="+data['meta']['after_cursor'])
+                display_tabulated_format(data['tickets'])
+                start_counter = start_counter + 25
+            
+            elif navigate == 'r':
+                return    
+            
+            else:    
+                print("Invalid input.")
+    except:
+        return
 
 
 if __name__ == "__main__":
@@ -115,14 +124,13 @@ if __name__ == "__main__":
         #Loading required credentials to connect to Zendesk API
         creds = get_creds()
 
+        #Welcome message
         print("Hi there!")
         print("Welcome to the ticket viewer")
         print("-----------------------------")
 
-        #user_option = input("Type 'menu' to view options or 'quit' (or any random input) to exit\n")
-
-        user_value = True #if user_option == "menu" else  False
-
+        user_option = input("Type 'menu' to view options or 'quit' (or any random input) to exit\n")
+        user_value = True if user_option == "menu" else  False
 
         while user_value:
             print_menu()
@@ -135,10 +143,9 @@ if __name__ == "__main__":
             elif(user_input=="3"):
                 display_25_tickets(creds)
             elif(user_input=="quit"):
-                break
+                user_value = False
             else:
                 print("Invalid input. Please re-enter a valid input.")    
     except:
         print("Ticket Viewer has ran into an error.")
-
     print("Thank you for using the ticket viewer. Goodbye.")
